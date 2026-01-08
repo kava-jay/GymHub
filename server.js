6de8a321502a93c43d601c36b97b779738357e20
Jay Prowerse <jay.kava@prowerse.com>
2026-01-08 13:01:02 +0530

    cors issue

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://gym-hub-pi.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
+
 // app.use(express.json());
 // Increase limit to 10mb to allow photos
 app.use(express.json({ limit: '10mb' }));
